c10bd2a02115ac9f8a3a8b9200bdd982cc6b5521
scsercan <canbolatsercan@gmail.com>
2022-01-21 16:18:06 +0300

    heroku deployment real time added.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  For Deployment
+  config.action_cable.allowed_request_origins = ['https://src-rails-demo-messaging.herokuapp.com/'] 
 end
