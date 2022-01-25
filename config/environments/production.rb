dcba0be954b4c0112aee42b1b3232988b8013e05
scsercan <canbolatsercan@gmail.com>
2022-01-25 17:45:06 +0300

    successfully verificate heroku account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  # config.action_cable.url = 'wss://src-rails-demo-messaging.herokuapp.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'https://src-rails-demo-messaging.herokuapp.com', /http:\/\/src-rails-demo-messaging.herokuapp.com.*/ ]
+  config.action_cable.url = 'wss://src-rails-demo-messaging.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://src-rails-demo-messaging.herokuapp.com', /http:\/\/src-rails-demo-messaging.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
