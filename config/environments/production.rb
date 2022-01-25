3fe7969c60ea0eaeae010ea04c3cbe7a57eb8aa2
scsercan <canbolatsercan@gmail.com>
2022-01-25 17:51:53 +0300

    heroku deploy ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,6 @@ Rails.application.configure do
   
   
   # For Deployment
-  # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.disable_request_forgery_protection = true
   config.action_cable.allowed_request_origins = ['https://src-rails-demo-messaging.herokuapp.com'] 
 end
